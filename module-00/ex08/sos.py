37a45ec1430be5518a46c34a779cf6dc9ea88e8c
Julien CAUCHETEUX <jdaodkql@gmail.com>
2022-10-28 22:10:13 +0200

    Ex08 module 00 done

---
+++ module-00/ex08/sos.py
@@ -0,0 +1,33 @@
+from re import A
+import sys
+if __name__ == "__main__":
+	if len(sys.argv) > 1:
+		morse_dict = { 'A':'.-', 'B':'-...',
+                    'C':'-.-.', 'D':'-..', 'E':'.',
+                    'F':'..-.', 'G':'--.', 'H':'....',
+                    'I':'..', 'J':'.---', 'K':'-.-',
+                    'L':'.-..', 'M':'--', 'N':'-.',
+                    'O':'---', 'P':'.--.', 'Q':'--.-',
+                    'R':'.-.', 'S':'...', 'T':'-',
+                    'U':'..-', 'V':'...-', 'W':'.--',
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+					'1':'.----', '2':'..---', '3':'...--',
+                    '4':'....-', '5':'.....', '6':'-....',
+                    '7':'--...', '8':'---..', '9':'----.'
+					}
+		s = ' '.join(filter(None, sys.argv[1:]))
+		if not all(c.isalnum() or c.isspace() for c in s):
+			print("ERROR")
+			exit(1)
+		s = s.upper()
+		s = s.replace(' ', '/')
+		s = ' '.join(s)
+		for c in s:
+			if c == ' ':
+				tmp = ' '
+			else:
+				for letter in morse_dict:
+					if c == letter:
+						tmp = morse_dict[letter]
+			print(tmp, end = "")
+		print()

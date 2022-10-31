866382df26fc5e0c1e61a1b6fbdb9b1ab1894771
julien-ctx <jdaodkql@gmail.com>
2022-10-31 18:20:08 +0100

    Vector dot done

--- module-01/ex02/test.py
+++ module-01/ex02/test.py
@@ -1,6 +1,6 @@
 from vector import Vector
 
 if __name__ == "__main__":
-	Test = Vector([[2.0], [9.0]])
-	print(str(Test.getShape()))
-
+	Test = Vector([[1.3], [2.9], [3.0]])
+	Test2 = Vector([[4.0], [5.7], [6.0]])
+	print (Test.dot(Test2))
--- module-01/ex02/vector.py
+++ module-01/ex02/vector.py
@@ -16,5 +16,22 @@ class Vector:
 			self.shape = (len(values), 1)
 		self.values = values
 
+	def dot(self, vec):
+		if not isinstance(vec, Vector):
+			print("Error: cannot execute dot product with a non-Vector variable")
+			return None
+		if self.shape == vec.getShape():
+			nb = 0.0
+			if self.shape[0] == 1:
+				for item1, item2 in zip(self.values[0], vec.values[0]):
+					nb += item1 * item2
+			else:
+				for item1, item2 in zip(self.values, vec.values):
+					nb += item1[0] * item2[0]
+			return nb
+		else:
+			print("Error: dot product cannot be executed if shapes are not the same")
+			return None
+
 	def getShape(self):
 		return self.shape

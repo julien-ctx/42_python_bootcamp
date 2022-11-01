4499168ac768f76879485d35f26a6cc4298383a7
julien-ctx <jdaodkql@gmail.com>
2022-11-01 21:09:01 +0100

    Generator almost finished in module 01

--- module-01/ex03/generator.py
+++ module-01/ex03/generator.py
@@ -1,12 +1,26 @@
-import string
+import string, random
 
 def generator(s = None, sep = " ", option = None):
-'''
-Splits the text according to sep value and yield the substrings.
-option precise if a action is performed to the substrings before it is yielded.
-'''
+	'''
+	Splits the text according to sep value and yield the substrings.
+	option precise if a action is performed to the substrings before it is yielded.
+	'''
 	if not s.isprintable():
 		print("Error: some characters are not printable.")
-	
+	s = s.split(sep)
+	if option == "shuffle":
+		print("ghnhng")
+		for item in s:
+			rand1 = random.randint(0, len(s) - 1)
+			rand2 = random.randint(0, len(s) - 1)
+			s[rand1], s[rand2] = s[rand2], s[rand1]
+	elif option == "unique":
+		s = sorted(set(s), key = s.index)
+	elif option == "ordered":
+		s = sorted(s)
+	print(s)
+
+
 	
 if __name__ == "__main__":
+	generator("calut Vest blol", " ", "ordered")

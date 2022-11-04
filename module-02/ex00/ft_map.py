e9888c8f4b80419b3f992580e414401a08206047
julien-ctx <jdaodkql@gmail.com>
2022-11-04 14:59:20 +0100

    Fixed map function

--- module-02/ex00/ft_map.py
+++ module-02/ex00/ft_map.py
@@ -1,20 +1,20 @@
+import sys
+
 def ft_map(f, i):
-	"""Map the function to all elements of the iterable.
-	Args:
-	f: a function taking an iterable.
-	i: an iterable object (list, tuple, iterator).
-	Return:
-	An iterable.
-	None if the iterable can not be used by the function.
 	"""
-	try:
-		iter(i)
-	except TypeError as e:
-		print("TypeError: " + str(e))
-		return None
+		Map the function to all elements of the iterable.
+		Args:
+		f: a function taking an iterable.
+		i: an iterable object (list, tuple, iterator).
+		Returns:
+		An iterable.
+		None if the iterable can not be used by the function.
+	"""
 	if callable(f):
-		return [f(x) for x in i]
+		try:
+			for x in i:
+				yield f(x)
+		except TypeError as e:  
+			sys.exit("TypeError: " + str(e))
 	else:
-		print("TypeError: f is not a function")
-		return None
- 
+		raise Exception("TypeError: f is not a function")

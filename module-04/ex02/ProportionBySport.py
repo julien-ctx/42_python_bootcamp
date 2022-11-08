7b251d9691da349f0240feb7ea24f7767adab8bf
Julien CAUCHETEUX <jdaodkql@gmail.com>
2022-11-08 10:19:48 +0100

    Filtering athlete exercise finished

---
+++ module-04/ex02/ProportionBySport.py
@@ -0,0 +1,10 @@
+from FileLoader import FileLoader
+
+def proportion_by_sport(df, year, sport, gender):
+	df = df[(df.Sex == gender) & (df.Year == year)].drop_duplicates(subset=["Name"], keep="first")
+	total = len(df)
+	return len(df[(df.Sex == gender) & (df.Sport == sport) & (df.Year == year)]) / total
+
+loader = FileLoader()
+data = loader.load("athlete_events.csv")
+print(proportion_by_sport(data, 2004, 'Tennis', 'F'))

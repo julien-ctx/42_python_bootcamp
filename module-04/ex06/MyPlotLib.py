fc1ba0cf056ee0b4be12910aa1b2bbf1167ae45c
julien-ctx <jdaodkql@gmail.com>
2022-11-09 13:46:58 +0100

    Seaborn pairplot finished

--- module-04/ex06/MyPlotLib.py
+++ module-04/ex06/MyPlotLib.py
@@ -1,7 +1,8 @@
 #https://towardsdatascience.com/feature-engineering-for-machine-learning-3a5e293a5114
+from FileLoader import FileLoader
 import matplotlib.pyplot as plt
 import matplotlib
-from FileLoader import FileLoader
+import seaborn as sns
 import scipy
 
 class MyPlotLib:
@@ -16,14 +17,17 @@ class MyPlotLib:
 		plt.show()
 
 	def density(self, df, features):
-		fig = plt.figure()
 		for feature in features:
 			df[feature].plot.density()
 			plt.legend(feature)
-		fig.tight_layout()
 		plt.show()
 
 	def pair_plot(self, df, features):
+		if len(features) != 2:
+			print("Error: pairplot must have a pair of values")
+			return
+		sns.pairplot(df, vars=[features[0], features[1]])
+		plt.show()
 		pass
 
 	def box_plot(self, df, features):
@@ -32,5 +36,6 @@ class MyPlotLib:
 loader = FileLoader()
 plot = MyPlotLib()
 data = loader.load("athlete_events.csv")
-plot.histogram(data, ["Height", "Weight"])
+# plot.histogram(data, ["Height", "Weight"])
 plot.density(data, ["Height", "Weight"])
+# plot.pair_plot(data, ["Weight", "Height"])

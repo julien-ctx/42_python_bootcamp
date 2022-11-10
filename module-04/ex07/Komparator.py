6655359b39f36695eaac6b34ac3873c2f8149f5f
julien-ctx <jdaodkql@gmail.com>
2022-11-10 10:45:57 +0100

    Python bootcamp finished except Kmean excercise

--- module-04/ex07/Komparator.py
+++ module-04/ex07/Komparator.py
@@ -37,7 +37,6 @@ class Komparator:
 			print("Using name as categorical variable is not relevant")
 			return
 		df = self.df.drop_duplicates(subset=['Name'], keep='first')
-		l = df[categorical_var].nunique()
 		all_cat = []
 		for cat in df[categorical_var]:
 			if cat not in all_cat:
@@ -48,10 +47,28 @@ class Komparator:
 		plt.show()
 
 	def compare_histograms(self, categorical_var, numerical_var):
-		pass
+		if categorical_var == 'Name':
+			print("Using name as categorical variable is not relevant")
+			return
+		df = self.df.drop_duplicates(subset=['Name'], keep='first')
+		l = df[categorical_var].nunique()
+		all_cat = []
+		fig = plt.figure()
+		i = 0
+		for cat in df[categorical_var]:
+			if cat not in all_cat:
+				i += 1
+				all_cat.append(cat)
+				ax = fig.add_subplot(1, l, i)
+				tmp = df[df[categorical_var] == cat]
+				tmp[numerical_var].hist()
+				ax.set_title(cat)
+		fig.tight_layout()
+		plt.show()	
 
 loader = FileLoader()
 data = loader.load("athlete_events.csv")
 komp = Komparator(data)
 # komp.compare_box_plots('Sex', 'Height')
-komp.density('Sex', 'Weight')
+# komp.density('Sex', 'Weight')
+# komp.compare_histograms('Sex', 'Weight')
